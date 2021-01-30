fb7ab873308cb43da821bb42f41fa6b7dcce01b2
YoramHernalsteen <r0582769@student.thomasmore.be>
2021-01-30 12:31:09 +0100

    adding week model

---
+++ database/migrations/2021_01_30_112444_create_weeks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeeksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weeks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('period_id');
+            $table->string('name');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+            $table->foreign('period_id')->references('id')->on('periods')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weeks');
+    }
+}
