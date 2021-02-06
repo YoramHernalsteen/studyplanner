3de4178c3db40b10b7dc1153b42268ce86737ecd
YoramHernalsteen <r0582769@student.thomasmore.be>
2021-02-06 21:27:43 +0100

    home_works table added

---
+++ database/migrations/2021_02_06_202307_create_home_works_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomeWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home_works', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('week_id');
+            $table->foreignId('course_id');
+            $table->string('name');
+            $table->boolean('done');
+            $table->date('date');
+            $table->timestamps();
+
+            $table->foreign('week_id')->references('id')->on('weeks')->onDelete('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home_works');
+    }
+}
