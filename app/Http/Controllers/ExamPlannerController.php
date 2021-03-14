6da0c71aecacc04522a58d3fce1a108887cad65d
YoramHernalsteen <r0582769@student.thomasmore.be>
2021-03-14 15:11:03 +0100

    new controllers to make exam planning possible

---
+++ app/Http/Controllers/ExamPlannerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ExamPlanner;
+use Illuminate\Http\Request;
+
+class ExamPlannerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ExamPlanner  $examPlanner
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ExamPlanner $examPlanner)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ExamPlanner  $examPlanner
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ExamPlanner $examPlanner)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ExamPlanner  $examPlanner
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ExamPlanner $examPlanner)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ExamPlanner  $examPlanner
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ExamPlanner $examPlanner)
+    {
+        //
+    }
+}
